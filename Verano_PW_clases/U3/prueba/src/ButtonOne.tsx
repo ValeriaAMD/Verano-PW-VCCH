7903a7df29a1f4b5dc65c6c57dde0ac24db3822f
ValeriaAMD <caro.campos251201@gmail.com>
2023-07-05 13:01:42 -0600

    no message

---
+++ Verano_PW_clases/U3/prueba/src/ButtonOne.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+export const ButtonOne = () => {
+  return (
+    <div>
+        <Button variant="dark">Dark</Button>
+    </div>
+  )
+}
